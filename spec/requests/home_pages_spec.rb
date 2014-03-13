6c87dcbb6766dbc4cd1eb3e6d774496251694768
Steve Jahns <s.t.jahns@gmail.com>
2014-03-12 23:18:34 -0600

    Test Cleanup

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -2,45 +2,34 @@ require 'spec_helper'
 
 describe "HomePages" do
 
+  subject { page }
+
   describe "Home page" do
-    it "should have the content 'Welcome to the Zeta Psi Fraternity'" do
-      visit '/'
-      expect(page).to have_content('Welcome to the Zeta Psi Fraternity')
-    end
 
-    it "should have the base title" do
-      visit '/'
-      expect(page).to have_title("Zeta Psi - Mu Theta")
-    end
+    before { visit root_path }
 
-    it "should not have custom title" do
-      visit '/'
-      expect(page).not_to have_title("Zeta Psi - Mu Theta | Home")
-    end
+    it { should have_content('Welcome') }
+    it { should have_title("Zeta Psi - Mu Theta") }
+    it { should_not have_title("Zeta Psi - Mu Theta | Home") }
+
   end
 
   describe "About page" do
-    it "should have the content 'The History of Zeta Psi'" do
-      visit '/about'
-      expect(page).to have_content('The History of Zeta Psi')
-    end
 
-    it "should have the right title" do
-      visit '/about'
-      expect(page).to have_title("Zeta Psi - Mu Theta | About")
-    end
+    before { visit '/about' }
+
+    it { should have_content('The History of Zeta Psi') }
+    it { should have_title("Zeta Psi - Mu Theta | About") }
+
   end
 
   describe "Contact page" do
-    it "should have the content 'please contact us directly'" do
-      visit '/contact'
-      expect(page).to have_content('please contact us directly')
-    end
 
-    it "should have the right title" do
-      visit '/contact'
-      expect(page).to have_title("Zeta Psi - Mu Theta | Contact")
-    end
+    before { visit '/contact' }
+
+    it { should have_content('please contact us directly') }
+    it { should have_title("Zeta Psi - Mu Theta | Contact") }
+
   end
 
 end

22c829a5f2e31248abcf37a9b6ce4a2fe58fe3be
Steve Jahns <s.t.jahns@gmail.com>
2014-03-14 01:47:07 -0600

    Mail config with environment variables

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,28 +1,15 @@
 require 'development_mail_interceptor'
 
-gmail_file = Rails.root.join('.gmail')
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => ENV["MAIL_USERNAME"],
+  :password             => ENV["MAIL_PASSWORD"],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 
-if File.exist?(gmail_file)
-
-  user_password_host = File.read(gmail_file).chomp.split(/\n/);
-  user = user_password_host[0];
-  password = user_password_host[1];
-  host = user_password_host[2];
-
-  ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => user,
-    :password             => password,
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
-
-  ActionMailer::Base.default_url_options[:host] = host
-
-else
-  raise "Missing .gmail config file in Rais root!"
-end
+ActionMailer::Base.default_url_options[:host] = ENV["MAIL_HOST"]
 
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -2,15 +2,9 @@ class DevelopmentMailInterceptor
 
   # Redirect mail to gmail account listed in .gmail
   def self.delivering_email(message)
-    gmail_file = Rails.root.join('.gmail')
-
-    if File.exist?(gmail_file)
-      user_password_host = File.read(gmail_file).chomp.split(/\n/);
-      user = user_password_host[0];
-
-      message.subject = "#{message.to} #{message.subject}"
-      message.to = "#{user}@gmail.com"
-    end
+    message.subject = "#{message.to} #{message.subject}"
+    puts "REDIRECTING TO: #{ENV['MAIL_USERNAME']}@gmail.com"
+    message.to = "#{ENV["MAIL_USERNAME"]}@gmail.com"
   end
 
 end

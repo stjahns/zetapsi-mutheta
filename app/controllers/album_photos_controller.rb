1a8a39ef520db8fd450976f99282149ae5caa2a3
Steve Jahns <s.t.jahns@gmail.com>
2014-03-18 15:12:09 -0600

    Improved photo album page

--- app/controllers/album_photos_controller.rb
+++ app/controllers/album_photos_controller.rb
@@ -5,10 +5,22 @@ class AlbumPhotosController < ApplicationController
     redirect_to edit_album_path(@album)
   end
 
+  def update
+    @album = Album.find(params[:album_id])
+    @photo = @album.album_photos.find(params[:id])
+    @photo.update(photo_params)
+    redirect_to edit_album_path(@album)
+  end
+
   def destroy
     @album = Album.find(params[:album_id])
     @photo = @album.album_photos.find(params[:id])
     @photo.destroy
     redirect_to edit_album_path(@album)
   end
+
+  private
+    def photo_params
+      params.require(:album_photo).permit(:caption)
+    end
 end
--- app/models/album_photo.rb
+++ app/models/album_photo.rb
@@ -3,8 +3,10 @@ class AlbumPhoto < ActiveRecord::Base
   has_attached_file :image,
     :styles=> {
       :frontpage => "1000x600#",
-      :medium => "300x300>",
-      :thumb => "200x250#" },
+      :medium => "1000x800>",
+      :thumb => "200x250#",
+      :navthumb => "150x150#" },
+    :convert_options => {:medium => "-background transparent -gravity center -extent 1000x800"},
     :default_url => "/assets/default_profilpic_200x250.gif"
   validates_attachment_content_type :image, :content_type => /image/
   validates :image, presence: true

b31dd83a84e1b5d8eecc1b695717354ee7ce7e5e
Steve Jahns <s.t.jahns@gmail.com>
2014-04-11 17:30:59 -0600

    expiring_url for reciepts hopefully uses https...

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,11 @@
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+
+module Paperclip
+  module Storage
+    module S3
+      def expiring_url(style = default_tyle, time = 3600)
+        s3_object(style).url_for(:read, :expires_in => time, :secure => true)
+      end
+    end
+  end
+end
